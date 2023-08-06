6882d9142452291d4e98f719815ee836f3eff49d
sanjaydell <sanjay.anuchuri@gmail.com>
2023-08-06 21:46:41 +0530

    updated book name style

--- client/src/components/BookName.tsx
+++ client/src/components/BookName.tsx
@@ -1,7 +1,7 @@
 export default function BookName(props: { title: string }) {
   const { title } = props;
   return (
-    <div className="name">
+    <div className="flex">
       <span className="font-semibold pl-2">{title}</span>
     </div>
   );
